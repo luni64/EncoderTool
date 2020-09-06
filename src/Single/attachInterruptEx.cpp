2920e4a491a552f5ee6c00b72f6b728e67ee1ba4
luni64 <lutz.niggl@lunoptics.com>
2020-09-06 08:09:09 +0200

    Changed attachInterruptEx to use a more flexible method to generate the relay function array

--- src/Single/attachInterruptEx.cpp
+++ src/Single/attachInterruptEx.cpp
@@ -1,52 +1,34 @@
 #include "attachInterruptEx.h"
 #include "core_pins.h"
 
-// This is a lame workaround for the problem that the Arduino attachInterrupt function does not allow passing any status to the callbacks.
-// attachInterruptEx allows to attach a std::function<void(void)> callback to the pin interrupts
-//
-// How does it work:
-// We generate an array of std::function<void(void)> to store the callbacks and an array of pointers to void functions as relays.
-// Since the relay functions are of type void(*f)() they can be used in the standard attachInterrupt function. The relays simply call the std::function callbacks stored in the 'cb' array.
-// Using lambdas to fill the array saves the hassle of explicitely declaring each of those functions.
+using namespace std;
 
-
-namespace // private namespace, avoid pollution
+namespace
 {
-    std::function<void(void)> cb[CORE_NUM_INTERRUPT];
+    // We need to attach a memberfunction of the encoder class to a pin interrupt. Since attachInterrupt only accepts  state less void(*)(void) callbacks
+    // this is not possible directly.
+    // Instead,we generate an array of void(*)() relay functions to attach to the pin interrupts. Those relays will invoke the actual callbacks.
+    // see https://stackoverflow.com/q/63730007/1842762
 
-    void (* const relays[CORE_NUM_INTERRUPT])(void)  // array of pointers to void(*relay)(void) functions (defined as lambda expressions)
+    function<void()> callbacks[NUM_DIGITAL_PINS];
+
+    template <unsigned nr>
+    constexpr void relay()
     {
-        [] { cb[0](); },  [] { cb[1](); },  [] { cb[2](); },  [] { cb[3](); },  [] { cb[4](); },
-        [] { cb[5](); },  [] { cb[6](); },  [] { cb[7](); },  [] { cb[8](); },  [] { cb[9](); },
-        [] { cb[10](); }, [] { cb[11](); }, [] { cb[12](); }, [] { cb[13](); }, [] { cb[14](); },
-        [] { cb[15](); }, [] { cb[16](); }, [] { cb[17](); }, [] { cb[18](); }, [] { cb[19](); },
-        [] { cb[20](); }, [] { cb[21](); }, [] { cb[22](); }, [] { cb[23](); }, [] { cb[24](); },
-        [] { cb[25](); }, [] { cb[26](); }, [] { cb[27](); }, [] { cb[28](); }, [] { cb[29](); },
-        [] { cb[30](); }, [] { cb[31](); }, [] { cb[32](); }, [] { cb[33](); },
-#if defined(ARDUINO_TEENSY31) || defined(ARDUINO_TEENSY32)
-    };
-#else
-        [] { cb[34](); }, [] { cb[35](); }, [] { cb[36](); }, [] { cb[37](); }, [] { cb[38](); }, [] { cb[39](); },
-#if defined(ARDUINO_TEENSY40)
-    };
-#else
-        [] { cb[40](); }, [] { cb[41](); }, [] { cb[42](); }, [] { cb[43](); }, [] { cb[44](); },
-        [] { cb[45](); }, [] { cb[46](); }, [] { cb[47](); }, [] { cb[48](); }, [] { cb[49](); },
-        [] { cb[50](); }, [] { cb[51](); }, [] { cb[52](); }, [] { cb[53](); }, [] { cb[54](); },
-#if defined(ARDUINO_TEENSY41)
-    };
-#else
-        [] { cb[55](); }, [] { cb[56](); }, [] { cb[57](); }, [] { cb[58](); }, [] { cb[59](); },
-        [] { cb[60](); }, [] { cb[61](); }, [] { cb[62](); }, [] { cb[63](); },
-    };
-#endif
-#endif
-#endif
-}; // namespace
+        callbacks[nr]();
+    }
 
+    template <unsigned... nr>
+    constexpr array<void (*)(), NUM_DIGITAL_PINS> MakeRelays(index_sequence<nr...>)
+    {
+        return array<void (*)(), NUM_DIGITAL_PINS>{relay<nr>...};
+    }
 
-void attachInterruptEx(unsigned pin, std::function<void(void)> callback, int mode)
+    constexpr auto relays = MakeRelays(make_index_sequence<NUM_DIGITAL_PINS>{});
+}
+
+void attachInterruptEx(unsigned pin, function<void(void)> callback, int mode)
 {
-    cb[pin] = callback;
+    callbacks[pin] = callback;
     attachInterrupt(pin, relays[pin], mode);
 }
